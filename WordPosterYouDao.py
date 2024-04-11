6403c34a4b18634fe88bcc097807a82d3b336389
aspack00 <3911925@qq.com>
2024-04-11 23:18:06 +0800

    增加urllib.parse.quote编码<等特殊字符串，已经能正常工作

--- WordPosterYouDao.py
+++ WordPosterYouDao.py
@@ -1,27 +1,30 @@
 import datetime
+import glob
+import html
 import json
 import os
-import time
 import shutil
-import glob
+import time
+import urllib
+
 import requests
 
 
 class WordPosterYouDao:
-    def __init__(self, file_prefix,):
+    def __init__(self, file_prefix, ):
         self.file_prefix = os.path.splitext(os.path.basename(file_prefix))[0]
         # 绝对路径
-        self.a_file_path = os.path.join(os.getcwd(), 'words', 'log',self.file_prefix)+'_.txt'
-        self.b_file_path = os.path.join(os.getcwd(), 'words', 'unfinished',self.file_prefix)+'.txt'
-        self.log_file_path = os.path.join(os.getcwd(), 'words', 'log',self.file_prefix)+'.log'
+        self.a_file_path = os.path.join(os.getcwd(), 'words', 'log', self.file_prefix) + '_.txt'
+        self.b_file_path = os.path.join(os.getcwd(), 'words', 'unfinished', self.file_prefix) + '.txt'
+        self.log_file_path = os.path.join(os.getcwd(), 'words', 'log', self.file_prefix) + '.log'
         self.line_num = 0
 
     def log_to_file_and_console(self, message):
-        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': '+message)
+        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ': ' + message)
         with open(self.log_file_path, 'a+', encoding='utf-8') as log_file:
-            log_file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+': '+message + '\n')
+            log_file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ': ' + message + '\n')
 
-    def replace_fran(self,string):
+    def replace_fran(self, string):
         fr_en = [['é', 'e'], ['ê', 'e'], ['è', 'e'], ['ë', 'e'], ['à', 'a'], ['â', 'a'], ['ç', 'c'], ['î', 'i'],
                  ['ï', 'i'],
                  ['ô', 'o'], ['ù', 'u'], ['û', 'u'], ['ü', 'u'], ['ÿ', 'y']
@@ -31,9 +34,10 @@ class WordPosterYouDao:
         return string
 
     def post_json_to_url(self, url, data):
-        headers = {'Content-Type': 'application/json'}
+        headers = {'Content-Type': 'application/json; charset=utf-8'}
+        proxy = {"http": None, "https": None}
         try:
-            response = requests.post(url, json=data, headers=headers)
+            response = requests.post(url, data=data, headers=headers, proxies=proxy)
             response.raise_for_status()
             return response.json()
         except (requests.exceptions.RequestException, json.JSONDecodeError) as e:
@@ -43,7 +47,7 @@ class WordPosterYouDao:
     # 移动文件
     def move_file(self):
         # 确定目标目录
-        target_directory = os.path.join(os.getcwd(),'words', 'finished')
+        target_directory = os.path.join(os.getcwd(), 'words', 'finished')
 
         # 如果目标目录不存在，则创建它
         if not os.path.exists(target_directory):
@@ -60,7 +64,7 @@ class WordPosterYouDao:
         for file_path in files_to_move:
             try:
                 # 目标文件路径
-                target_file_path = os.path.join(target_directory,os.path.basename(file_path))
+                target_file_path = os.path.join(target_directory, os.path.basename(file_path))
                 # 移动文件
                 shutil.move(file_path, target_file_path)
                 self.log_to_file_and_console(f"文件 {file_path} 已成功移动到 {target_directory}")
@@ -89,13 +93,14 @@ class WordPosterYouDao:
                 # 从第start_line_number行开始读取
                 for line in lines[A_NUM:]:
                     if len(line.strip()) > 0 and ('#' not in line):
-                        words.append(self.replace_fran(line.strip()))
-                    if len(words) == 10 or line == lines[-1]:
+                        # words.append(self.replace_fran(line.strip()))
+                        words.append(line.strip())
+                    if len(words) == 5 or line == lines[-1]:
                         if len(words) == 0:
                             return
                         self.log_to_file_and_console(f"Starting to send words from line {self.line_num + 1}.")
-                        data = '{"wordlist":[' + ','.join(words)+']}'
-
+                        data = urllib.parse.quote('{"wordlist":[' + ','.join(words) + ']}')
+                        # gg = json.loads(data)
                         # 重试POST请求最多3次
                         b_success = False
                         for attempt in range(100):
@@ -107,11 +112,12 @@ class WordPosterYouDao:
                                 if code == 0:
                                     # 更新起始行号
                                     self.line_num += 10
-                                    with open(self.a_file_path, 'w',encoding='utf-8') as a_file:
+                                    with open(self.a_file_path, 'w', encoding='utf-8') as a_file:
                                         a_file.write(str(self.line_num))
                                     b_success = True
                                     words.clear()
-                                    self.log_to_file_and_console(f"该文件 {self.file_prefix} 成功: {self.line_num} /{len(lines)}行")
+                                    self.log_to_file_and_console(
+                                        f"该文件 {self.file_prefix} 成功: {self.line_num} /{len(lines)}行")
                                     break  # 请求成功，跳出重试循环
                                 else:
                                     self.log_to_file_and_console(f"POST response with non-zero code: {body}")
@@ -122,7 +128,7 @@ class WordPosterYouDao:
                         if not b_success:
                             # 失败则退出
                             self.log_to_file_and_console(
-                                f"program exit,max tried {attempt+1}times from start num: {self.line_num + 1} to end num: {self.line_num + 10 + 1}.")
+                                f"program exit,max tried {attempt + 1}times from start num: {self.line_num + 1} to end num: {self.line_num + 10 + 1}.")
                             return
                 # 处理完的文件挪到finished文件夹
                 self.log_to_file_and_console("文件处理完成，准备挪到finished文件夹.")
@@ -141,8 +147,3 @@ if __name__ == "__main__":
     post_url = "https://tcb-df22zhkzi34p4nm-4cvje34891ec.service.tcloudbase.com/add_update_words_youdao"  # 替换为实际的URL
     word_poster = WordPosterYouDao("all.txt")
     word_poster.read_words_and_post(post_url)
-
-
-
-
-
